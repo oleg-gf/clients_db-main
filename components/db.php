931901529b09f0f9a89a4cacd052c81f0c96479e
oleg-gf <oleg_gf@mail.ru>
2022-04-08 18:06:31 +0300

    Update db.php

--- components/db.php
+++ components/db.php
@@ -9,9 +9,4 @@ $dbname = "clientsdb";
 $dbuser = "clientsdb"; 
 $dbpass = "clientsdb"; 
 $dbtable = "clients";
-$mysqli = new mysqli(DBHOST, DBNAME, DBUSER, DBPASS);
-$mysqli->set_charset("utf-8");
 
-if ($mysqli->connect_error) {
-  die("Не удалось подключиться к БД ".$mysqli->connect_error);
-}
